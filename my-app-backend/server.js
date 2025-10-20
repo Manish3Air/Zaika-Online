f007a98195cff8dbfde53f5a775fc2138bc2ef73
Manish3air <manishpandey3365@gmail.com>
2025-10-20 09:31:39 +0530

    prod url added in cors

--- my-app-backend/server.js
+++ my-app-backend/server.js
@@ -36,7 +36,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 const allowedOrigins = [
   'http://localhost:3000',    // e.g. http://localhost:3000
-  'https://yourproductiondomain.com',   // e.g. https://yourproductiondomain.com
+  'https://zaika-online.vercel.app',   // e.g. https://yourproductiondomain.com
 ];
 
 app.use(cors({
