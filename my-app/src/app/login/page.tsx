b9406efcac2ffd91d6ff6de0a920236106c74453
Manish3air <manishpandey3365@gmail.com>
2025-10-20 09:48:50 +0530

    google url redirected fixed

--- my-app/src/app/login/page.tsx
+++ my-app/src/app/login/page.tsx
@@ -2,7 +2,7 @@ export default function LoginPage() {
   return (
     <div className="flex justify-center mt-20">
       <a
-        href="http://localhost:5000/api/auth/google"
+        href={`${process.env.NEXT_PUBLIC_API_BASE}/api/auth/google`}
         className="px-6 py-3 bg-blue-600 text-white rounded-lg text-lg"
       >
         Login with Google
