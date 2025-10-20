4e2cf443c151da019d23b4eb979d2b4cb52e555c
Manish3air <manishpandey3365@gmail.com>
2025-10-20 09:33:45 +0530

    prod url added in cors

--- my-app-backend/server.js
+++ my-app-backend/server.js
@@ -42,6 +42,7 @@ const allowedOrigins = [
 app.use(cors({
   origin: function (origin, callback) {
     // Allow requests with no origin (like Postman, mobile apps)
+    
     if (!origin) return callback(null, true);
     if (allowedOrigins.includes(origin)) {
       return callback(null, true);
